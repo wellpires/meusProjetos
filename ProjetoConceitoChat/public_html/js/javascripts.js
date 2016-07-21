bb9ca66dd6180d98b32c40f1fd4d53af5a50e290
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2016-07-21 17:51:34 -0300

    Atualização

--- ProjetoConceitoChat/public_html/js/javascripts.js
+++ ProjetoConceitoChat/public_html/js/javascripts.js
@@ -1,52 +1,62 @@
 function sendMessage() {
-    var inputMessage = document.getElementById('btn-input');
-    var usuario = 'wellington.goncalves.pires@everis.com';
+    var inputMessage = $('#btn-input');
+    var usuario = 'usuario963';
+    var novoID = '';
+    var novoIDIntervalo = '';
 
     $.get('view/newMessage.html', function (data, status, response) {
-        data = data.replace(/#_MENSAGEM_USUARIO_#/, inputMessage.value);
+        var lastID = findLastID();
+        var lastInterval = findLastIDIntervalo();
+        novoID = 'ID_MINUTO_' + (++lastID);
+        novoIDIntervalo = 'INTERVALO_' + (++lastInterval);
+        data = data.replace(/#_MENSAGEM_USUARIO_#/, inputMessage.val());
         data = data.replace(/#_NOME_USUARIO_#/, usuario);
-        $('#box_messages_sents').append(data);
+        data = data.replace(/#_ID_MINUTO_#/g, novoID);
+        data = data.replace(/#_INTERVALO_#/g, novoIDIntervalo);
+
+        var _html = $(data);
+        _html.children().children().children()[1].id = novoID;
+        $('#box_messages_sents').append(_html);
+        inputMessage.val('');
     });
+
 }
-function ativarMinutos() {
-    setInterval(function () {
-        var minutos = document.getElementById('minutos');
-        var minutosInteiro = minutos.innerHTML;
-        var horas = '';
-        var dia = '';
-        var tempoFormatado = '';
-        var regexp = /d|h/;
-        if (regexp.test(minutosInteiro)) {
-            var horaArray = '';
-            if (/h/.test(minutosInteiro)) {
-                horaArray = minutosInteiro.split('h');
-            }
-            horas = horaArray[0];
-            minutosInteiro = parseInt(horaArray[1]);
-        }
-        minutosInteiro++;
-        if (minutosInteiro > 59) {
-            minutosInteiro = 0;
-            horas++;
-        }
-        if (horas.toString().length > 0) {
-            if (horas > 23) {
-                dia++;
-            }
 
-            if (horas.toString().length < 2) {
-                horas = '0' + horas.toString();
-            }
-            horas = horas.toString() + 'h';
-        }
-        if (minutosInteiro.toString().length < 2) {
-            minutosInteiro = '0' + minutosInteiro;
-        }
+function findLastID() {
 
+    var data = $(document).contents()[1].innerHTML;
+    var result = '';
 
-        tempoFormatado = horas.toString() + minutosInteiro.toString();
+    var regexp = /ID_MINUTO_\d{1,}/gi;
+    var ids = data.match(regexp);
+    if (!ids)
+        return -1;
+    ids.sort(function (s1, s2) {
+        var s1lower = s1.toLowerCase();
+        var s2lower = s2.toLowerCase();
+        return s1lower > s2lower ? 1 : (s1lower < s2lower ? -1 : 0);
+    });
+    regexp = /\d{1,}/;
+    result = parseInt(ids[ids.length - 1].match(regexp));
 
-        minutos.innerHTML = tempoFormatado;
+    return result;
+}
+function findLastIDIntervalo() {
 
-    }, 1000);
+    var data = $(document).contents()[1].innerHTML;
+    var result = '';
+
+    var regexp = /INTERVALO_\d{1,}/gi;
+    var ids = data.match(regexp);
+    if (!ids)
+        return -1;
+    ids.sort(function (s1, s2) {
+        var s1lower = s1.toLowerCase();
+        var s2lower = s2.toLowerCase();
+        return s1lower > s2lower ? 1 : (s1lower < s2lower ? -1 : 0);
+    });
+    regexp = /\d{1,}/;
+    result = parseInt(ids[ids.length - 1].match(regexp));
+
+    return result;
 }
